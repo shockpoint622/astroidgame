43926321060d2f2c6ae35377a810d4fe4ce6fe4f
shockpoint <epyonascend622@gmail.com>
2026-07-19 21:33:27 -0400

    Asteroids Project started. Game window opens and inital loop set

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    #main game loop
+    while True:
+        #log the current state of the game in jsonl file
+        log_state()
+        #logic for closing/quitting the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #blank starting screen for initial testing perposes
+        screen.fill("black")
+        #refreshes the screen at the end of the loop
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
