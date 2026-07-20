82c4a41e5aa08321e9eae98623f4a94fa720dc0d
shockpoint <epyonascend622@gmail.com>
2026-07-19 22:20:46 -0400

    Added base Player Framework

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -16,8 +16,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         #blank starting screen for initial testing perposes
         screen.fill("black")
+        user: Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        user.draw(screen)
         #refreshes the screen at the end of the loop
         dt = clock.tick(60) / 1000
         pygame.display.flip()
