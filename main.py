59f5d1d789ba983dd413666556b78961019143df
shockpoint <epyonascend622@gmail.com>
2026-07-20 23:42:56 -0400

    Added Asteroids to the Asteroids game

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -10,8 +13,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     user: Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field: AsteroidField = AsteroidField()
 
     #main game loop
     while True:
